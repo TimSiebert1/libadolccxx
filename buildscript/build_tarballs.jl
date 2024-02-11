4bdcd60272fdce3307e411a35deea25b381fbba5
TimSiebert1 <tim.siebert96@outlook.com>
2024-02-11 23:09:34 +0100

    searching the bug

--- buildscript/build_tarballs.jl
+++ buildscript/build_tarballs.jl
@@ -11,14 +11,14 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "ADOLC"
-version = v"0.0.13"
+version = v"0.0.14"
 
 isyggdrasil = get(ENV, "YGGDRASIL", "") == "true"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/coin-or/ADOL-C.git", "26460017f9ed2f016d63b9e620d6146eb5d7c855"),
-    GitSource("https://github.com/TimSiebert1/libadolccxx.git", "933064cfb554a5a3f5e72ce0442c1e3d5722761d"),
+    GitSource("https://github.com/TimSiebert1/libadolccxx.git", "6b39296446a5661c7aeda70b75dbb6aa721efc75"),
 ]
 
 
